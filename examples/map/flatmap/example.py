40796e55a4cd40e4f7058b637d7782630f61840e
Ibnu Muhammad <ibnu0muhammad@gmail.com>
2024-05-14 17:26:34 +0700

    Add print()

--- examples/map/flatmap/example.py
+++ examples/map/flatmap/example.py
@@ -1,6 +1,6 @@
 from pynumaflow.mapper import Messages, Message, Datum, MapServer, Mapper
 
-from pynumaflow._constants import _LOGGER
+# from pynumaflow._constants import _LOGGER
 
 
 class Flatmap(Mapper):
@@ -10,23 +10,24 @@ class Flatmap(Mapper):
     """
 
     def handler(self, keys: list[str], datum: Datum) -> Messages:
-        _LOGGER.info("type(datum)...")
-        _LOGGER.info(type(datum))
-        _LOGGER.info("type(datum.value)...")
-        _LOGGER.info(type(datum.value))
-        _LOGGER.info("type(datum.event_time)...")
-        _LOGGER.info(type(datum.event_time))
-        _LOGGER.info("type(datum.watermark)...")
-        _LOGGER.info(type(datum.watermark))
+        print("coba_print...")
+        # _LOGGER.info("type(datum)...")
+        # _LOGGER.info(type(datum))
+        # _LOGGER.info("type(datum.value)...")
+        # _LOGGER.info(type(datum.value))
+        # _LOGGER.info("type(datum.event_time)...")
+        # _LOGGER.info(type(datum.event_time))
+        # _LOGGER.info("type(datum.watermark)...")
+        # _LOGGER.info(type(datum.watermark))
         val = datum.value
         _ = datum.event_time
         _ = datum.watermark
         strs = val.decode("utf-8").split(",")
-        _LOGGER.info("type(strs)...")
-        _LOGGER.info(type(strs))
+        # _LOGGER.info("type(strs)...")
+        # _LOGGER.info(type(strs))
         messages = Messages()
-        _LOGGER.info("type(messages)...")
-        _LOGGER.info(type(messages))
+        # _LOGGER.info("type(messages)...")
+        # _LOGGER.info(type(messages))
         if len(strs) == 0:
             messages.append(Message.to_drop())
             return messages
