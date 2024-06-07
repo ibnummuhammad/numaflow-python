cdfb43c717fa1793fd20800a7e5db95da4ae274f
Ibnu Muhammad <ibnu0muhammad@gmail.com>
2024-06-07 21:19:49 +0700

    Fix sink integration between numaflow to postgres via kafka

--- examples/map/flatmap/example.py
+++ examples/map/flatmap/example.py
@@ -1,3 +1,5 @@
+import json
+
 from pynumaflow.mapper import Messages, Message, Datum, MapServer, Mapper
 
 # from pynumaflow._constants import _LOGGER
@@ -26,6 +28,12 @@ class Flatmap(Mapper):
         datum_str = datum.value.decode("utf-8")
         print("datum_str...")
         print(datum_str)
+        pesan = val.decode("utf-8")
+        print("pesan...")
+        print(pesan)
+        pesan_dict = json.loads(pesan)
+        print("pesan_dict...")
+        print(pesan_dict)
         strs = val.decode("utf-8").split(",")
         print("strs...")
         print(strs)
@@ -33,10 +41,20 @@ class Flatmap(Mapper):
         print("messages...1")
         print(messages)
         nilai = '{ "schema": { "type": "struct", "fields": [ { "type": "struct", "fields": [ { "type": "string", "optional": false, "field": "params" }, { "type": "string", "optional": false, "field": "payload" }, { "type": "string", "optional": false, "field": "etl_id" }, { "type": "string", "optional": false, "field": "etl_id_ts" }, { "type": "string", "optional": false, "field": "etl_id_partition" }, { "type": "string", "optional": false, "field": "run_ts" } ], "optional": true, "name": "dbserver1.inventory.customers.Value", "field": "before" }, { "type": "struct", "fields": [ { "type": "string", "optional": false, "field": "params" }, { "type": "string", "optional": false, "field": "payload" }, { "type": "string", "optional": false, "field": "etl_id" }, { "type": "string", "optional": false, "field": "etl_id_ts" }, { "type": "string", "optional": false, "field": "etl_id_partition" }, { "type": "string", "optional": false, "field": "run_ts" } ], "optional": true, "name": "dbserver1.inventory.customers.Value", "field": "after" }, { "type": "struct", "fields": [ { "type": "string", "optional": false, "field": "version" }, { "type": "string", "optional": false, "field": "connector" }, { "type": "string", "optional": false, "field": "name" }, { "type": "int64", "optional": false, "field": "ts_ms" }, { "type": "string", "optional": true, "name": "io.debezium.data.Enum", "version": 1, "parameters": { "allowed": "true,last,false,incremental" }, "default": "false", "field": "snapshot" }, { "type": "string", "optional": false, "field": "db" }, { "type": "string", "optional": true, "field": "sequence" }, { "type": "string", "optional": true, "field": "table" }, { "type": "int64", "optional": false, "field": "server_id" }, { "type": "string", "optional": true, "field": "gtid" }, { "type": "string", "optional": false, "field": "file" }, { "type": "int64", "optional": false, "field": "pos" }, { "type": "int32", "optional": false, "field": "row" }, { "type": "int64", "optional": true, "field": "thread" }, { "type": "string", "optional": true, "field": "query" } ], "optional": false, "name": "io.debezium.connector.mysql.Source", "field": "source" }, { "type": "string", "optional": false, "field": "op" }, { "type": "int64", "optional": true, "field": "ts_ms" }, { "type": "struct", "fields": [ { "type": "string", "optional": false, "field": "id" }, { "type": "int64", "optional": false, "field": "total_order" }, { "type": "int64", "optional": false, "field": "data_collection_order" } ], "optional": true, "name": "event.block", "version": 1, "field": "transaction" } ], "optional": false, "name": "dbserver1.inventory.customers.Envelope", "version": 1 }, "payload": { "before": null, "after": { "params": "banco", "payload": "iben", "etl_id": "muhammad", "etl_id_ts": "ibnu_muhammad@gmail.com", "etl_id_partition": "partisi", "run_ts": "lari" }, "source": { "version": "2.1.4.Final", "connector": "mysql", "name": "dbserver1", "ts_ms": 1717568433000, "snapshot": "false", "db": "inventory", "sequence": null, "table": "customers", "server_id": 1, "gtid": null, "file": "binlog.000002", "pos": 2645, "row": 0, "thread": 23, "query": null }, "op": "c", "ts_ms": 1717568433831, "transaction": null } }'
+        nilai_dict = json.loads(nilai)
+        print("nilai_dict...1")
+        print(nilai_dict)
+        nilai_dict["payload"]["after"]["params"] = pesan_dict["payload"]["after"]["first_name"]
+        nilai_dict["payload"]["after"]["payload"] = pesan_dict["payload"]["after"]["last_name"]
+        print("nilai_dict...2")
+        print(nilai_dict)
+        nilai_str = json.dumps(nilai_dict)
+        print("nilai_str...")
+        print(nilai_str)
         if len(strs) == 0:
             messages.append(Message.to_drop())
         else:
-            messages.append(Message(str.encode(nilai)))
+            messages.append(Message(str.encode(nilai_str)))
         print("messages...2")
         print(messages)
         return messages
